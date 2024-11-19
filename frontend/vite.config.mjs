efbd846100cca26820ec92e7dd18693ce7747864
SamuelT1995 <samueltenagne1995@gmail.com>
2024-11-19 08:51:06 +0300

    frontend

---
+++ frontend/vite.config.mjs
@@ -0,0 +1,7 @@
+// Rename vite.config.js to vite.config.mjs
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+});
